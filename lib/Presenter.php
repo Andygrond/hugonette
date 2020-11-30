25e36e372fc51b489b50dab4838c086897c4521c
Andygrond <grond@vp.pl>
2020-11-30 13:07:18 +0100

    Provider is now an extension of Presenter

--- lib/Presenter.php
+++ lib/Presenter.php
@@ -4,40 +4,29 @@ namespace Andygrond\Hugonette;
 
 /* MVP Presenter class for Hugonette
  * methods of Presenter extension class will return an array of model data
- * when Presenter method is empty, next route will be checked
+ * when Presenter method returns false (means: route not relevant), next route will be checked
  * @author Andygrond 2020
 **/
 
-class Presenter
+abstract class Presenter
 {
   protected $model = []; // base model data
 
-  public function __construct()
-  {
-  }
-
-  /**
-  * @return: view object according to view strategy defined in Route
-  */
-  final protected function viewStrategy()
-  {
-    $viewClass = Env::get('namespace.view') .ucfirst(Env::get('view')) .'View';
-    return new $viewClass();
-  }
-
-  /** view model data calculated by presenter class:method declared in router
+  /** calculate Model data using Presenter class:method declared in router and pass it to View
   * @param method = presenter method name determined in route definition
   */
   final public function run(string $method)
   {
     $model = $this->$method();
+
     if (false !== $model) {
       if (is_array($model)) {
-        $this->viewStrategy()->view($this->model + $model);
+        $view = Env::get('namespace.view') .ucfirst(Env::get('view')) .'View';
+        (new $view)->view($this->model + $model);
         Log::close(); // effective only when set previously
         exit;
       } else {
-        throw new \TypeError("Please return array or false in " .get_class($this) ."->$method()");
+        throw new \TypeError(get_class($this) ."::$method returned " .gettype($model) .". Please return array or false.");
       }
     }
   }
